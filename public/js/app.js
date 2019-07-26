d2abecc53c70966290dab3527ad355b5f8092c2b
User <pargev@aimtech.am>
2019-07-26 16:35:28 +0400

    u

--- public/js/app.js
+++ public/js/app.js
@@ -65295,8 +65295,11 @@ var SpreadTheWorldComponent = function SpreadTheWorldComponent() {
 
 var VinLookupComponent = function VinLookupComponent() {
   return __webpack_require__.e(/*! import() */ 3).then(__webpack_require__.bind(null, /*! ../components/VinLookupComponent */ "./resources/js/components/VinLookupComponent.vue"));
-}; // const UnsubscribeComponent = () => import('../components/UnsubscribeComponent');
+};
 
+var UnsubscribeComponent = function UnsubscribeComponent() {
+  return __webpack_require__.e(/*! import() */ 6).then(__webpack_require__.bind(null, /*! ../components/UnsubscribeComponent */ "./resources/js/components/UnsubscribeComponent.vue"));
+};
 
 vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]);
 var routes = [{
@@ -65314,6 +65317,9 @@ var routes = [{
 }, {
   path: '/vin-lookup',
   component: VinLookupComponent
+}, {
+  path: '/unsubscribe',
+  component: UnsubscribeComponent
 }];
 var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
   mode: 'history',
