43c14be88cc41052dec5da104fcd78f7c3c8eaa8
User <pargev@aimtech.am>
2019-07-30 15:59:35 +0400

    asds

--- resources/js/components/SpreadTheWorldComponent.vue
+++ resources/js/components/SpreadTheWorldComponent.vue
@@ -18,7 +18,7 @@
                     <div class="social-row">
                         SHARE THIS STORY:
 
-                        <div class="fb-share-button" :data-href="baseUrl" data-layout="button_count" data-size="small"><a target="_blank" :href="'https://www.facebook.com/sharer/sharer.php?u='+baseUrl+'/#share'+i+'&amp;src=sdkpreparse'" class="fb-xfbml-parse-ignore"><img src="/site/images/fb-icon.png" alt="icon"></a></div>
+                        <div class="fb-share-button" :data-href="baseUrl + '/spread-the-world/#share' + i" data-layout="button_count" data-size="small"><a target="_blank" :href="'https://www.facebook.com/sharer/sharer.php?u='+baseUrl+'/spread-the-world/#share'+i+'&amp;src=sdkpreparse'" class="fb-xfbml-parse-ignore"><img src="/site/images/fb-icon.png" alt="icon"></a></div>
 <!--                        <a :href="'https://www.facebook.com/sharer/sharer.php?u=' + baseUrl + '/storage/' + video.video" target="_blank"><img src="/site/images/fb-icon.png" alt="icon"> </a>-->
                         <a :href="'https://twitter.com/intent/tweet?text=' + baseUrl + '/storage/' + video.video"><img src="/site/images/twitter-icon.png" alt="icon"> </a>
                     </div>
