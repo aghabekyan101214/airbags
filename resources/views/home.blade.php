8229892a1f99dceb30c85c7ccb5d2213fa43303a
User <pargev@aimtech.am>
2019-07-30 12:31:46 +0400

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,6 +42,10 @@
 
     <meta property="og:url"           content="{{ url('/spread-the-world') }}" />
     <meta property="og:image" content="{{ asset('site/images/video-img-desktop.png') }}">
+    <meta property="og:image:secure_url" content="{{ asset('site/images/video-img-desktop.png') }}" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="400" />
+    <meta property="og:image:height" content="300" />
     <meta property="og:type" content="video/mp4">
     <meta property="og:title" content="simple title">
     <meta property="og:description" content="simple description">
