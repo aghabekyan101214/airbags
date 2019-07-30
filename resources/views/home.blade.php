fb460255e67a539e8551a3704bfa32ea4ffef247
User <pargev@aimtech.am>
2019-07-30 14:35:25 +0400

    asd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,13 +57,8 @@
 </noscript>
 <!-- End Google Tag Manager (noscript) -->
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v4.0"></script>
+
 <div id="app">
     <layout></layout>
 </div>
