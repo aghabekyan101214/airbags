05ad16f9b62be68abe2895ad9c8b8680ac1d494e
User <pargev@aimtech.am>
2019-08-09 14:01:18 +0400

    saa

--- app/Http/Controllers/FbShareController.php
+++ app/Http/Controllers/FbShareController.php
@@ -15,27 +15,11 @@ class FbShareController extends Controller
             'app_secret' => '0f5c79dfe5ad65281d8a68976742552d',
             'default_graph_version' => 'v2.10',
         ]);
-        $data = [
-            'title' => 'Airbag recall',
-            'description' => 'Takata airbag recall',
-            'source' => $fb->videoToUpload(asset('/storage/y9NVQQRcdp2oU00bVCGu051JPVpdWzHJBL7SA6aN.mp4')),
-        ];
+        $helper = $fb->getRedirectLoginHelper();
 
-        try {
-            $response = $fb->post('/me/videos', $data, 'user-access-token');
-        } catch(Facebook\Exceptions\FacebookResponseException $e) {
-            // When Graph returns an error
-            echo 'Graph returned an error: ' . $e->getMessage();
-            exit;
-        } catch(Facebook\Exceptions\FacebookSDKException $e) {
-            // When validation fails or other local issues
-            echo 'Facebook SDK returned an error: ' . $e->getMessage();
-            exit;
-        }
+        $permissions = ['email']; // Optional permissions
+        $loginUrl = $helper->getLoginUrl('https://example.com/fb-callback.php', $permissions);
 
-        $graphNode = $response->getGraphNode();
-        var_dump($graphNode);
-
-        echo 'Video ID: ' . $graphNode['id'];
+        echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
     }
 }
